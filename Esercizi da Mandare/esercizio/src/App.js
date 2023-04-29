82c149f6fd1e577f2099207d3a74b2164bace95a
RobertoFanara <fanararoberto7@gmail.com>
2023-04-29 16:36:54 +0200

    esercizio 7 completato

--- Esercizi da Mandare/esercizio/src/App.js
+++ Esercizi da Mandare/esercizio/src/App.js
@@ -1,5 +1,5 @@
-const Welcome = ({name = 'Alessandro', age}) => {
-  return (
+const Welcome = ({name = <strong>Alessandro</strong>, age}) => {    // What happens if the name prop is a JSX expression instead of a string?
+  return (                                                          // the name will be bold
     <>
       <p>Welcome, {name}!</p>
       <p>Your age is, {age = 33}</p>
