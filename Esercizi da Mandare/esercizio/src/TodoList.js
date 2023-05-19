d6b326639e6dffd1384a307f17f7921d734016a5
RobertoFanara <fanararoberto7@gmail.com>
2023-05-19 18:12:58 +0200

    esercizio 27 completato

--- Esercizi da Mandare/esercizio/src/TodoList.js
+++ Esercizi da Mandare/esercizio/src/TodoList.js
@@ -1,15 +1,25 @@
 import React, {useState} from "react";
+import {useRef} from "react"
 
 export default function TodoList() {
+
+  const input = useRef(null)
   const [name, setName] = useState('');
   const [items, setItems] = useState(["Strawberry Cake", "Profiterols", "Chocolate"]);
   const handleAdd = () => {
       setItems([...items, name]);
-  };
+      input.current.value = ""
+    };
+  const removeAll = () => {
+    setItems([]);
+  }
+
   return (
       <div>
-          <input onChange={(e) => setName(e.target.value)}/>
+          <input onChange={(e) => setName(e.target.value)} ref={input}/>
+          
           <button onClick={handleAdd}>add</button>
+          <button onClick={removeAll}>remove all</button>
           {items.map((item, index) => {return <li index={index}>{item}</li>})}
       </div>
   );
