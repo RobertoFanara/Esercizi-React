226fd1e7d4b0a4a50e295a809972645e564591ec
RobertoFanara <fanararoberto7@gmail.com>
2023-05-20 21:02:07 +0200

    esercizio 31 completato

--- Esercizi da Mandare/esercizio/src/App.js
+++ Esercizi da Mandare/esercizio/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import DisplayLanguage, { LanguageContext } from "./DisplayLanguage";
+import DisplayLanguage from "./DisplayLanguage";
+import { LanguageContext } from "./LanguageContext"
 
 export default class App extends React.Component {
   state = {
--- Esercizi da Mandare/esercizio/src/DisplayLanguage.js
+++ Esercizi da Mandare/esercizio/src/DisplayLanguage.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { LanguageContext } from "./LanguageContext"
+
+const Strings = {
+  en:{
+    GREETINGS: "Hello, Welcome"
+  },
+  it:{
+    GREETINGS: "Ciao, Benvenuto"
+  }
+}
 
 export default class DisplayLanguage extends React.Component {
     render(){
         return (
           <>
-          ciao!
+          <LanguageContext.Consumer>
+            {Language => {
+                return(
+                  <div>
+                    <h1>
+                      {Strings[Language].GREETINGS}
+                    </h1>
+                  </div>
+                )
+              }
+            }
+
+          </LanguageContext.Consumer>
           </>
         )
       }
