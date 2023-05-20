c7a54ccaa3d95c78285c3dd8d2262c8c6ca2a5ab
RobertoFanara <fanararoberto7@gmail.com>
2023-05-20 21:02:55 +0200

    esercizio 32 completato

--- Esercizi da Mandare/esercizio/src/App.js
+++ Esercizi da Mandare/esercizio/src/App.js
@@ -1,30 +1,9 @@
-import React from "react";
-import DisplayLanguage from "./DisplayLanguage";
-import { LanguageContext } from "./LanguageContext"
+import Clickcounter from "./Clickcounter"
 
-export default class App extends React.Component {
-  state = {
-    language: "en",
-  };
-
-  handleLanguageChange = (event) => {
-    this.setState({
-      language: event.target.value,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <select value={this.state.language} onChange={this.handleLanguageChange}>
-          <option value="en">ENGLISH</option>
-          <option value="it">ITALIANO</option>
-        </select>
-        <LanguageContext.Provider value={this.state.language}>
-        <DisplayLanguage />
-        </LanguageContext.Provider>
-      
-      </div>
-    );
-  }
+export default function App() {
+    return(
+        <>
+        <Clickcounter />
+        </>
+    )
 }
