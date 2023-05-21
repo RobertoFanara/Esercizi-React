c0ad80e65a1a5db6ee8fd75cbf37f73d625064aa
RobertoFanara <fanararoberto7@gmail.com>
2023-05-21 20:52:13 +0200

    esercizio 39 completato

--- Esercizi da Mandare/esercizio/src/App.js
+++ Esercizi da Mandare/esercizio/src/App.js
@@ -1,10 +1,16 @@
-import Clickcounter from "./Clickcounter"
-const App = () => {
-    return(
-        <>
-        <Clickcounter />
-        </>
-    )
+import React from "react"
+import useStoreInputs from "./useStoreInputs";
+
+const HandleForm = () => {
+  const {onLogin, autoComplete, autoComplete2} = useStoreInputs()
+
+  return (
+    <form>
+      <input type="text" onChange={autoComplete} />
+      <input type="password" onChange={autoComplete2} />
+      <button onClick={onLogin} type="submit">Login</button>
+    </form>
+  );
 }
 
-export default App
+export default HandleForm
