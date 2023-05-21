e155ac702e4eb58c04d053666f60436b483ec726
RobertoFanara <fanararoberto7@gmail.com>
2023-05-21 23:32:30 +0200

    esercizio 53 completato

--- Esercizi da Mandare/esercizio/src/useGithubUser.js
+++ Esercizi da Mandare/esercizio/src/useGithubUser.js
@@ -3,7 +3,7 @@ import useSWR from "swr"
 const fetcher = url => fetch(url).then(response => response.json())
 
 const useGithubUser = (username) => {
-    const {data} = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
+    const {data, error} = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
 
     return {
         data,
