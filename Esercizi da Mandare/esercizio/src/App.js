1c8f6590653f9accc3714a2fbdb12b7c5ee0aef2
RobertoFanara <fanararoberto7@gmail.com>
2023-05-21 22:36:27 +0200

    esercizio 48 completato

--- Esercizi da Mandare/esercizio/src/App.js
+++ Esercizi da Mandare/esercizio/src/App.js
@@ -1,5 +1,6 @@
 import { Routes } from "react-router-dom"
 import { Route } from "react-router-dom"
+import { Link } from "react-router-dom"
 import ShowGithubUser from "./ShowGithubUser"
 
 const App = () => {
@@ -7,8 +8,11 @@ const App = () => {
     return(
         <>
         <Routes>
-            <Route path="users/:username" element={<ShowGithubUser />} />
+            <Route path="users/:username" element={<ShowGithubUser />} />       
         </Routes>
+        <p><Link to="users/yangus98">Link to my repo...</Link></p>
+        <p><Link to="users/rushizo">Link to my friend Danilo...</Link></p>
+        <p><Link to="users/Lucone00">Link to my friend Luca...</Link></p>
         </>
     )
 }
